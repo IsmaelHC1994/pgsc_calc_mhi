6fce4f00e3581d24410f116ae17a2e50afeca2bc
ismaelhc1994 <ismaelhc94@gmail.com>
2025-09-30 14:59:03 -0400

    launch single version as on ICA for more testing

---
+++ launch_gvcf_hpc_single.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+# this is recommended by the developers to add to the .sh 
+export NXF_ANSI_LOG=false
+export NXF_OPTS="-Xms500M -Xmx2G"
+
+# Create log directory if it doesn't exist
+mkdir -p logsSingle
+
+# Define log file with timestamp
+LOG_FILE="logsSingle/pgsc_calc_gvcf_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+module load nextflow
+module load apptainer
+
+# Run the Nextflow command and pipe output to tee for both display and logging
+nextflow run "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/main_with_gvcf.nf" \
+    -c "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/nextflow.config" \
+    -profile apptainer,narval \
+    --sampleset gvcfTestsingle \
+    --gvcf_files "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/24-1979.hard-filtered.gvcf.gz" \
+    --reference_genome "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/hg38.fa.gz" \
+    --scorefile_custom "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/scores/scores.tar.gz" \
+    --report_template "/home/ihcasti/wd/mhi-prs/run_pgsc_2_01/bin/patient_report_template.qmd" \
+    --target_build GRCh38 \
+    --genotypes_cache cache \
+    --run_ancestry /home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/pgsc_HGDP+1kGP_v1.tar.zst \
+    --liftover \
+    --hg19_chain /home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/hg19ToHg38.over.chain.gz \
+    --hg38_chain /home/ihcasti/wd/mhi-prs/run_pgsc_2_01/assets/qc/hg38ToHg19.over.chain.gz \
+    --target_scores_report "brugadaMTAG" \
+    -resume \
+    -with-trace traceMHI_gvcf_$(date +%Y%m%d_%H%M%S).txt 2>&1 | tee "${LOG_FILE}"
